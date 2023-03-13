dd672c598a55dcb9299282b7c75070e8113a62ea
Brandon Coston <costonb@gmail.com>
2023-03-12 18:12:06 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Brandon Coston on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
